b0c91152471b13cb04c3bf9643daec4e57fa130e
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-09 10:54:45 +0300

    Modified lab

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,12 @@ import ArticleList from "./ArticleList";
 console.log(blogData);
 
 function App() {
+  const { name, image, about, posts } = blogData;
   return (
     <div className="App">
+      <Header name={name} />
+      <About image={image} about={about} />
+      <ArticleList posts={posts} />
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,16 +1,21 @@
-import React from "react";
-import Article from "./Article";
+// src/components/ArticleList.js
+import React from 'react';
+import Article from './Article';
 
-function ArticleList({ articles }) {
-  const articleItems = articles.map((article) => (
-    <Article
-      key={article.id}
-      title={article.title}
-      date={article.date}
-      preview={article.preview}
-    />
-  ));
-  return (<main>{articleItems}</main>);
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post, index) => (
+        <Article 
+          key={index}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+          minutes={post.minutes}
+        />
+      ))}
+    </main>
+  );
 }
 
 export default ArticleList;
